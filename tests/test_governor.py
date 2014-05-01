03639a73d2d609711e34e3b34d41d66d9b0d33b5
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2014-05-01 14:09:30 -0400

    tests(governor): update for new algorithm
    Updates and refactors tests for great good.

--- tests/test_governor.py
+++ tests/test_governor.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 from __future__ import division
+import contextlib
 import io
 import logging
 import multiprocessing
@@ -30,12 +31,49 @@ import requests
 import tests
 
 
-def make_rate(slimit, hlimit, methods=None,
-              route=None, period_sec=1, node_count=1):
+def run_server(app, host, port):
+    httpd = simple_server.make_server(host, port, app)
+    httpd.serve_forever()
+
+
+def _suppress_logging():
+    requests_log = logging.getLogger("requests")
+    rlevel = requests_log.level
+    requests_log.setLevel(logging.WARNING)
+    stdtmp, sys.stderr = sys.stderr, io.BytesIO()  # Suppress logging
+    return stdtmp, rlevel,
+
+
+def _recover_logging(stdtmp, rlevel):
+    requests_log = logging.getLogger("requests")
+    requests_log.setLevel(rlevel)
+    sys.stderr = stdtmp
+
+
+@contextlib.contextmanager
+def make_silent_server(app, host, port):
+    stdtmp, rlevel = _suppress_logging()
+    process = multiprocessing.Process(
+        target=run_server, args=(app, host, port)
+    )
+    process.daemon = True
+    process.start()
+
+    # Give the process a moment to start up
+    time.sleep(0.1)
+
+    yield process
+
+    process.terminate()
+    _recover_logging(stdtmp, rlevel)
+
+
+def make_rate(limit, methods=None,
+              route=None, drain_velocity=1.0):
     req = [
         ('name', str(uuid.uuid4())),
-        ('soft_limit', slimit),
-        ('hard_limit', hlimit)
+        ('limit', limit),
+        ('drain_velocity', drain_velocity)
     ]
     return governor.Rate(dict(
         req + (
@@ -43,7 +81,7 @@ def make_rate(slimit, hlimit, methods=None,
         ) + (
             [('route', route)] if route is not None else []
         )
-    ), period_sec, node_count)
+    ))
 
 
 class DTuple(tuple):
@@ -79,25 +117,18 @@ class TestGovernor(tests.util.TestCase):
         self.governor = governor.wrap(tests.util.app)
 
         config = governor.CONF['eom:governor']
-        self.node_count = config['node_count']
-        self.period_sec = config['period_sec']
-        rates = governor._load_rates(config['rates_file'],
-                                     self.period_sec, self.node_count)
+        rates = governor._load_rates(config['rates_file'])
 
         self.test_rate = rates[0]
-        self.soft_limit = self.test_rate.soft_limit
-        self.hard_limit = self.test_rate.hard_limit
+        self.limit = self.test_rate.limit
         self.test_url = '/v1/queues/fizbit/messages'
+        self.limiter = governor._create_limiter({})
 
         self.default_rate = rates[1]
 
-    def _quantum_leap(self):
-        # Wait until the next time quantum
-        normalized = time.time() % (self.period_sec * 2)
-        if normalized < self.period_sec:
-            time.sleep(self.period_sec - normalized)
-        else:
-            time.sleep(self.period_sec * 2 - normalized)
+    def tearDown(self):
+        super(TestGovernor, self).tearDown()
+        self.limiter = governor._create_limiter({})
 
     def test_missing_project_id(self):
         env = self.create_env('/v1')
@@ -109,28 +140,13 @@ class TestGovernor(tests.util.TestCase):
         self.governor(env, self.start_response)
         self.assertEqual(self.status, '204 No Content')
 
-    def test_soft_limit(self):
-        self._test_limit(self.soft_limit, 204)
-
-    def test_soft_limit_burst(self):
-        self._test_limit(self.soft_limit, 204, burst=True)
-
-    def test_soft_limit_default(self):
-        self._test_limit(self.default_rate.soft_limit, 204, 'PATCH')
-
-    def test_hard_limit(self):
-        self._test_limit(self.hard_limit, 429)
-
-    def test_hard_limit_burst(self):
-        self._test_limit(self.hard_limit, 429, burst=True)
-
     @ddt.data(
         # (rate, method, expect)
-        method_annotate(make_rate(100, 200), "GET", True),
-        method_annotate(make_rate(100, 200, ["GET"]), "GET", True),
-        method_annotate(make_rate(100, 200, ["GET"]), "POST", False),
-        method_annotate(make_rate(100, 200, ["GET", "POST"]), "POST", True),
-        method_annotate(make_rate(100, 200, ["GET", "POST"]), "PUT", False)
+        method_annotate(make_rate(200), "GET", True),
+        method_annotate(make_rate(200, ["GET"]), "GET", True),
+        method_annotate(make_rate(200, ["GET"]), "POST", False),
+        method_annotate(make_rate(200, ["GET", "POST"]), "POST", True),
+        method_annotate(make_rate(200, ["GET", "POST"]), "PUT", False)
     )
     def test_applies_to_method(self, data):
         rate, method, expect = data
@@ -138,107 +154,77 @@ class TestGovernor(tests.util.TestCase):
 
     @ddt.data(
         # (rate, route, expect)
-        route_annotate(make_rate(100, 200, route="/"), "/", True),
-        route_annotate(make_rate(100, 200, route="/"), "/v1", False),
-        route_annotate(make_rate(100, 200, route="/v1.*"), "/v1", True),
-        route_annotate(make_rate(100, 200, route="/v1.*"), "/v1/queues", True),
-        route_annotate(make_rate(100, 200, route="/v1.*"), "/v2/queues", False)
+        route_annotate(make_rate(200, route="/"), "/", True),
+        route_annotate(make_rate(200, route="/"), "/v1", False),
+        route_annotate(make_rate(200, route="/v1.*"), "/v1", True),
+        route_annotate(make_rate(200, route="/v1.*"), "/v1/queues", True),
+        route_annotate(make_rate(200, route="/v1.*"), "/v2/queues", False)
     )
     def test_applies_to_route(self, data):
         rate, route, expect = data
         self.assertEqual(governor.applies_to(rate, [], route), expect)
 
     def test_match_rate_gives_preference_to_project_specific(self):
-        expect = make_rate(1, 2)
+        expect = make_rate(1)
         prates = {'11': expect}
-        grates = [make_rate(1, 2), make_rate(3, 4)]
+        grates = [make_rate(2), make_rate(4)]
         self.assertEqual(
             governor.match_rate('11', None, None, prates, grates),
             expect
         )
 
     def test_match_rate_returns_general_rate_otherwise(self):
-        expect = make_rate(1, 2)
-        prates = {'11': make_rate(1, 2)}
-        grates = [expect, make_rate(3, 4)]
+        expect = make_rate(2)
+        prates = {'11': make_rate(2)}
+        grates = [expect, make_rate(4)]
         self.assertEqual(
             governor.match_rate('12', None, None, prates, grates),
             expect
         )
 
     def test_match_rate_returns_none_when_none_applicable(self):
-        prates = {'11': make_rate(1, 2)}
-        grates = [make_rate(1, 2, ['GET']), make_rate(1, 2, ['DELETE'])]
+        prates = {'11': make_rate(2)}
+        grates = [make_rate(2, ['GET']), make_rate(1, ['DELETE'])]
         self.assertIsNone(
             governor.match_rate('12', 'PUT', None, prates, grates)
         )
+
+    def test_limiter_raises_if_over_limit(self):
+        call = lambda: self.limiter(1, self.test_rate)
+        [call() for _ in range(self.limit+1)]
+        self.assertRaises(governor.HardLimitError, call)
+
+    def test_limit_reached_no_429(self):
+        self._test_limit(self.default_rate.limit, 204)
+
+    def test_limit_surpassed_leads_to_429(self):
+        self._test_limit(self.default_rate.limit + 3, 429)
+
+    def test_draining_evades_429(self):
+        self._test_draining(self.default_rate.limit + 3, 204)
 
     #----------------------------------------------------------------------
     # Helpers
     #----------------------------------------------------------------------
 
     def _test_limit(self, limit, expected_status,
-                    http_method='GET', burst=False):
+                    method='GET'):
+        request = getattr(requests, method.lower())
+        host, port = '127.0.0.1', 8783
+        with make_silent_server(self.governor, host, port):
+            url = 'http://%s:%s' % (host, port) + self.test_url
+            call = lambda: request(url, headers={'X-Project-ID': 1234})
+            resps = [call().status_code for _ in range(limit)]
+            self.assertEqual(resps[-1], expected_status)
 
-        request = getattr(requests, http_method.lower())
-
-        def run_server():
-            sys.stderr = io.BytesIO()  # Suppress logging
-
-            httpd = simple_server.make_server('127.0.0.1', 8783,
-                                              self.governor)
-            httpd.serve_forever()
-
-        process = multiprocessing.Process(target=run_server)
-        process.daemon = True
-        process.start()
-
-        # Suppress logging
-        requests_log = logging.getLogger("requests")
-        requests_log.setLevel(logging.WARNING)
-
-        # Give the process a moment to start up
-        time.sleep(0.1)
-
-        num_periods = 5
-        sec_per_req = self.period_sec / limit
-        url = 'http://127.0.0.1:8783' + self.test_url
-
-        # Start out at the beginning of a time bucket
-        self._quantum_leap()
-
-        if burst:
-            for i in range(int(limit) + 100):
-                request(url, headers={'X-Project-ID': 1234})
-
-            self._quantum_leap()
-
-        start = time.time()
-        stop_1 = start + self.period_sec
-        stop_N = start + self.period_sec * num_periods
-
-        responses = []
-        while time.time() < stop_1:
-            resp = request(url, headers={'X-Project-ID': 1234})
-            responses.append(resp.status_code)
-            time.sleep(sec_per_req * .7)
-
-        num_requests = 0
-        while time.time() < stop_N:
-            resp = request(url, headers={'X-Project-ID': 1234})
-            responses.append(resp.status_code)
-            num_requests += 1
-            time.sleep(sec_per_req * .7)
-
-        if expected_status == 204:
-            # We would have slept so we can predict
-            # the rate.
-            expected = limit * (num_periods - 1)
-
-            self.assertAlmostEqual(num_requests, expected,
-                                   delta=(150 / self.node_count))
-            self.assertNotIn(429, responses[1:])
-        else:
-            self.assertIn(429, responses)
-
-        process.terminate()
+    def _test_draining(self, limit, expected_status,
+                       method='GET'):
+        request = getattr(requests, method.lower())
+        host, port = '127.0.0.1', 8783
+        with make_silent_server(self.governor, host, port):
+            url = 'http://%s:%s' % (host, port) + self.test_url
+            call = lambda: request(url, headers={'X-Project-ID': 1234})
+            [call().status_code for _ in range(limit//2)]
+            time.sleep(0.5)
+            resp = [call().status_code for _ in range(limit//2)][-1]
+            self.assertEqual(resp, expected_status)
