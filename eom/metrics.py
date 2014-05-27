a05ddda7735b527232805dabb6eb03b6abce1a5b
Alex Bettadapur <alexbettadapur@gmail.com>
2014-05-27 16:44:47 -0400

    Added some namespacing

--- eom/metrics.py
+++ eom/metrics.py
@@ -19,23 +19,24 @@ def wrap(app):
     def middleware(env, start_response):
 
         def _start_response(status, headers, *args):
-            statuscode = int(status[:3])
-            if statuscode / 500 == 1:
-                client.incr("500")
-            elif statuscode / 400 == 1:
-                client.incr("400")
-            elif statuscode / 200 == 1:
-                client.incr("200")
-            client.incr("requests")
+            status_code = int(status[:3])
+            if status_code / 500 == 1:
+                client.incr("requests.500")
+            elif status_code / 400 == 1:
+                client.incr("requests.400")
+            elif status_code / 200 == 1:
+                client.incr("requests.200")
+            client.incr("requests.total")
 
             return start_response(status, headers, *args)
 
+        request_method = env["REQUEST_METHOD"]
         start = time.time() * 1000
         response = app(env, _start_response)
         stop = time.time() * 1000
 
         elapsed = stop - start
-        client.timing("latency", elapsed)
+        client.timing("latency."+request_method, elapsed)
         return response
 
     return middleware
