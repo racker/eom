f6d96262a93bb1aadd2841a0fc4826457ba98723
TheSriram <sriram@klusterkloud.com>
2014-05-27 08:52:14 -0400

    feat(redis utils) : redis connection utility added

---
+++ eom/utils/redis_pool.py
@@ -0,0 +1,19 @@
+import redis
+
+from oslo.config import cfg
+
+CONF = cfg.CONF
+
+REDIS_GROUP_NAME = 'eom:redis'
+OPTIONS = [
+    cfg.StrOpt('host'),
+    cfg.StrOpt('port'),
+]
+
+CONF.register_opts(OPTIONS, group=REDIS_GROUP_NAME)
+
+
+def get_client():
+    group = CONF[REDIS_GROUP_NAME]
+    pool = redis.ConnectionPool(host=group['host'], port=group['port'], db=0)
+    return redis.Redis(connection_pool=pool)
